7bc565d293f65b9a4d604c3cdbc391e1b7383cbe
Noah Broyles <noah@javamate.net>
2020-11-01 18:27:11 -0500

    Added comments

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@
 
 'use strict';
 
+
 class Quiz {
 
+    // delcare all the class properties
     questions;
     currentQuestionIndex;
     totalQuestions;
     correctAnswers;
-    incorrectAnswers;
 
     constructor(questions) {
         this.questions = questions; 
         this.currentQuestionIndex = 0;
         this.totalQuestions = Object.keys(questions).length;
         this.correctAnswers = 0;
-        this.incorrectAnswers = 0;
     }
 
     take() {
@@ -31,21 +31,24 @@ class Quiz {
 
         let currentQuestion = Object.keys(this.questions)[this.currentQuestionIndex];
         document.getElementById('current-question').innerHTML = currentQuestion;
+        // populate the answer section with the options for this question
         for (let answer in this.questions[currentQuestion]) {
             document.getElementById('answer-section').innerHTML += `<label for="${this.questions[currentQuestion][answer]}">${this.questions[currentQuestion][answer]}</label><input name="answer" type="radio" value="${this.questions[currentQuestion][answer]}"><br>`;
         }
     }
 
+    // display the next question
     nextQuestion() {
         this.currentQuestionIndex += 1; // we are moving along through the questions
         if (this.currentQuestionIndex === this.totalQuestions) {
             // the quiz is over.
             document.getElementById('quiz-form').innerHTML= `<h2>Yay! The quiz is over!</h2><p>Your score is ${(this.correctAnswers/this.totalQuestions) * 100}%.</p>`;
-            return;
+            return; // don't call take() again!
         }
         this.take();
     }
 
+    // checks if an answers matches the question
     checkAnswer(question, answer) {
         if (this.questions[question]["correct"] == answer) {
             return true;
@@ -58,15 +61,20 @@ class Quiz {
 let q;
 
 function submitQuestion () {
-    let answer = document.querySelector('input[name="answer"]:checked').value;
-    let question = document.getElementById('current-question').innerHTML;
-    if (q.checkAnswer(question, answer)) {
-        q.correctAnswers += 1;
-    } else {
-        q.incorrectAnswers += 1;
+    try {
+        // get the question and answer
+        let answer = document.querySelector('input[name="answer"]:checked').value;
+        let question = document.getElementById('current-question').innerHTML;
+
+        if (q.checkAnswer(question, answer)) {
+            q.correctAnswers += 1;
+        }
+        q.nextQuestion();
+        return false;
+        
+    } catch { // they didn't fill in the answer
+        alert("Please select an answer.");
     }
-    q.nextQuestion();
-    return false;
 }
 
 window.onload = function() {
@@ -76,10 +84,10 @@ window.onload = function() {
         "What is 7 * 6?": {0:"46", 1:"67", "correct":"42", 2:"76"},
         "How many times does 1 go into 8?": {0:"81", 1:"18", 2:"6", "correct":"8"},
         "Is the square root of 42 a whole number?": {0:"yes", "correct":"no", 1:"Maybe So"},
-        "Who wrote the song <em>Shape of You</em>?": {"correct":"Ed Sheeran", 0:"Justin Bieber", 1:"Someone Else"}
+        "Why is 1764 special?": {"correct":"Because its the square of 42", 0:"Because it's prime", 1:"Because it's over 1000"}
     }
     q = new Quiz(questions);
-    q.take(); // start taking the quiz
+    q.take(); // start the quiz
 }
 
 
